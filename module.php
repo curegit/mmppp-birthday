bfe8f606b799c49d4091d6d32faa5edb0ad47554
curegit <37978051+curegit@users.noreply.github.com>
2023-03-27 22:33:43 +0900

    Initial commit

---
+++ module.php
@@ -0,0 +1,22 @@
+<?php
+if ($main = !defined("MAIN")) {
+    define("MAIN", __FILE__);
+}
+
+$data = file_get_contents("birthday.csv");
+$eol = "\r\n";
+$sep = ",";
+
+$header = strtok($data, $eol);
+$keys = explode($sep, $header);
+$records = array();
+while (($line = strtok($eol)) !== false) {
+    $values = explode($sep, $line);
+    $records[] = array_combine($keys, $values);
+}
+
+if ($main) {
+    header('Content-Type: text/plain;charset=UTF-8');
+    header('Cache-Control: no-store');
+    var_dump($records);
+}
